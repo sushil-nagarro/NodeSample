bc0fd6468588aac07b9d24db5dbc4bb13e5d793c
sushil-nagarro <94160986+sushil-nagarro@users.noreply.github.com>
2023-04-27 21:13:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const express = require('express');
-
+// const express = require('express');
+console.log('Hello World')
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// // App
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+// app.listen(PORT, HOST, () => {
+//   console.log(`Running on http://${HOST}:${PORT}`);
+// });
